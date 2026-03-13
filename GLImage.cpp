7036b56ee85525765c5dbd527c1b641772fb4b41
lyazj <seeson@pku.edu.cn>
2026-03-14 02:42:02 +0800

    Fix reference to stb

--- GLImage.cpp
+++ GLImage.cpp
@@ -9,8 +9,8 @@
 #ifdef HAS_STB
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image.h>
-#include <stb/stb_image_write.h>
+#include <stb_image.h>
+#include <stb_image_write.h>
 #endif /* HAS_STB */
 
 #include <iostream>
