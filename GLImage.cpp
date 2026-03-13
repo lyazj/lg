0090eaf03fe58695c24303e2d5427a9448d52244
小小佳的云朵儿～ <seeson@pku.edu.cn>
2026-03-13 10:03:08 +0800

    Use nullptr for GLImage save format pointer

--- GLImage.cpp
+++ GLImage.cpp
@@ -20,12 +20,12 @@ void GLImage::Load(const fs::path &path [[maybe_unused]])
 #ifdef HAS_MAGICK
   // We do need .string() on Windows unfortunately...
   Magick::Image img(path.string());
+  img.flip();
   width = (GLint)img.columns(), height = (GLint)img.rows();
   type = img.matte() ? GLImageType::RGBA : GLImageType::RGB;
   data.resize(width * height * (type == GLImageType::RGBA ? 4 : 3));
   const char *format = type == GLImageType::RGBA ? "RGBA" : "RGB";
   img.write(0, 0, width, height, format, Magick::CharPixel, data.data());
-  img.flip();
 #else  /* HAS_MAGICK */
   cerr << "Warning: GLImage::Load() unavailable" << endl;
 #endif /* HAS_MAGICK */
@@ -34,7 +34,7 @@ void GLImage::Load(const fs::path &path [[maybe_unused]])
 void GLImage::Save(const fs::path &path [[maybe_unused]]) const
 {
 #ifdef HAS_MAGICK
-  const char *format = NULL;
+  const char *format = nullptr;
   switch(type) {
   case GLImageType::RGB: format = "RGB"; break;
   case GLImageType::RGBA: format = "RGBA"; break;
