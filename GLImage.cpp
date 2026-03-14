dc094ca335ea7d484507e547ed85846e8d0a9cba
lyazj <seeson@pku.edu.cn>
2026-03-14 15:54:09 +0800

    Add minimal builtin BMP saving

--- GLImage.cpp
+++ GLImage.cpp
@@ -13,6 +13,7 @@
 #include <stb_image_write.h>
 #endif /* HAS_STB */
 
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -108,7 +109,7 @@ void GLImage::Backend::GLImageSTBBackend::Load(GLImage &image, const fs::path &p
     return;
   }
   image.type = GLImageType::RGBA;
-  image.data.assign((const byte *)d, (const byte *)d + image.width * image.height * n);
+  image.data.assign((const byte *)d, (const byte *)d + image.width * image.height * 4);
   stbi_image_free(d);
 }
 
@@ -160,7 +161,7 @@ void GLImage::Backend::Init(GLImageBackend backend)
   switch(backend) {
   case GLImageBackend::Default:
     gInstance = make_unique<Backend>();
-    cerr << "Warning: no GLImage backend available, using dummy implementation" << endl;
+    cerr << "Warning: no GLImage backend available, using minimal builtin implementation" << endl;
     break;
 
 #ifdef HAS_MAGICK
@@ -175,12 +176,108 @@ void GLImage::Backend::Init(GLImageBackend backend)
   }
 }
 
+namespace {
+
+enum Endian { LittleEndian, BigEndian };
+
+inline Endian ProbeEndian()
+{
+  uint16_t x = 1;
+  return *(const uint8_t *)&x == 1 ? LittleEndian : BigEndian;
+}
+
+uint16_t ToLittleEndian16(uint16_t x)
+{
+  if(ProbeEndian() == LittleEndian) return x;
+  return (x >> 8) | (x << 8);
+}
+
+uint32_t ToLittleEndian32(uint32_t x)
+{
+  if(ProbeEndian() == LittleEndian) return x;
+  return ((x >> 24) & 0x000000FF) | ((x >> 8) & 0x0000FF00) | ((x << 8) & 0x00FF0000) | ((x << 24) & 0xFF000000);
+}
+
+#pragma pack(push, 1)
+struct BMPFileHeader {
+  uint16_t bfType = 0x4D42;  // "BM"
+  uint32_t bfSize = 0;       // Change me.
+  uint16_t bfReserved1 = 0, bfReserved2 = 0;
+  uint32_t bfOffBits = 54;
+
+  void ToLittleEndian();
+};
+struct BMPInfoHeader {
+  uint32_t biSize = 40;
+  int32_t biWidth = 0, biHeight = 0;  // Change us.
+  uint16_t biPlanes = 1;
+  uint16_t biBitCount = 32;  // RGBA.
+  uint32_t biCompression = 0;
+  uint32_t biSizeImage = 0;                                // Change me.
+  int32_t biXPelsPerMeter = 2835, biYPelsPerMeter = 2835;  // 72 DPI
+  uint32_t biClrUsed = 0, biClrImportant = 0;
+
+  void ToLittleEndian();
+};
+#pragma pack(pop)
+
+static_assert(sizeof(BMPFileHeader) == 14);
+static_assert(sizeof(BMPInfoHeader) == 40);
+
+void BMPFileHeader::ToLittleEndian()
+{
+  bfType = ToLittleEndian16(bfType);
+  bfSize = ToLittleEndian32(bfSize);
+  bfReserved1 = ToLittleEndian16(bfReserved1);
+  bfReserved2 = ToLittleEndian16(bfReserved2);
+  bfOffBits = ToLittleEndian32(bfOffBits);
+}
+
+void BMPInfoHeader::ToLittleEndian()
+{
+  biSize = ToLittleEndian32(biSize);
+  biWidth = ToLittleEndian32(biWidth);
+  biHeight = ToLittleEndian32(biHeight);
+  biPlanes = ToLittleEndian16(biPlanes);
+  biBitCount = ToLittleEndian16(biBitCount);
+  biCompression = ToLittleEndian32(biCompression);
+  biSizeImage = ToLittleEndian32(biSizeImage);
+  biXPelsPerMeter = ToLittleEndian32(biXPelsPerMeter);
+  biYPelsPerMeter = ToLittleEndian32(biYPelsPerMeter);
+  biClrUsed = ToLittleEndian32(biClrUsed);
+  biClrImportant = ToLittleEndian32(biClrImportant);
+}
+
+}  // namespace
+
 void GLImage::Backend::Load(GLImage &, const fs::path &) const
 {
   cerr << "Warning: no GLImage backend available, Load() is a no-op" << endl;
 }
 
-void GLImage::Backend::Save(const GLImage &, const fs::path &) const
+void GLImage::Backend::Save(const GLImage &image, const fs::path &path) const
 {
-  cerr << "Warning: no GLImage backend available, Save() is a no-op" << endl;
+  BMPFileHeader fileHeader;
+  BMPInfoHeader infoHeader;
+  fileHeader.bfSize = fileHeader.bfOffBits + image.width * image.height * 4;
+  infoHeader.biWidth = image.width;
+  infoHeader.biHeight = image.height;
+  infoHeader.biSizeImage = image.width * image.height * 4;
+  fileHeader.ToLittleEndian();
+  infoHeader.ToLittleEndian();
+
+  ofstream file(path, ios::binary);
+  file.write((const char *)&fileHeader, sizeof(fileHeader));
+  file.write((const char *)&infoHeader, sizeof(infoHeader));
+  int n = image.type == GLImageType::RGBA ? 4 : 3;
+  for(GLint y = 0; y < image.height; y++) {
+    for(GLint x = 0; x < image.width; x++) {
+      const byte *p = &image.data[n * (y * image.width + x)];
+      file.write((const char *)(p + 2), 1);                    // B
+      file.write((const char *)(p + 1), 1);                    // G
+      file.write((const char *)(p + 0), 1);                    // R
+      file.write(n == 4 ? (const char *)(p + 3) : "\xff", 1);  // A
+    }
+  }
+  if(!file) cerr << "Error saving image: " << path << endl;
 }
