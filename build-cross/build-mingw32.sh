1f8784c27ddcde4f96ec5ced161f80c16627253a
lyazj <seeson@pku.edu.cn>
2026-03-30 20:01:16 +0800

    Improve building system

--- build-cross/build-mingw32.sh
+++ build-cross/build-mingw32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -S . -B build-mingw32 -DCMAKE_TOOLCHAIN_FILE=${PWD}/mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-mingw32 &&
+cmake -S .. -B build-mingw32 -DCMAKE_TOOLCHAIN_FILE=${PWD}/mingw32.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-mingw32 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF &&
 cmake --build build-mingw32 --parallel $(nproc) &&
 cmake --install build-mingw32 --strip
--- build-cross/build-mingw64.sh
+++ build-cross/build-mingw64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -S . -B build-mingw64 -DCMAKE_TOOLCHAIN_FILE=${PWD}/mingw64.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-mingw64 &&
+cmake -S .. -B build-mingw64 -DCMAKE_TOOLCHAIN_FILE=${PWD}/mingw64.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-mingw64 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF &&
 cmake --build build-mingw64 --parallel $(nproc) &&
 cmake --install build-mingw64 --strip
