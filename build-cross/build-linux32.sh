1c72ba2d966c42e7350e7959b55e7addceafd722
lyazj <seeson@pku.edu.cn>
2026-04-02 10:55:09 +0800

    Update ci scripts

--- build-cross/build-linux32.sh
+++ build-cross/build-linux32.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -S .. -B build-linux32 -DCMAKE_TOOLCHAIN_FILE=${PWD}/linux32.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-linux32 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF &&
+cmake -S .. -B build-linux32 -DCMAKE_TOOLCHAIN_FILE=${PWD}/linux32.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-linux32 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF -DUSE_ICU=OFF &&
 cmake --build build-linux32 --parallel $(nproc) &&
 cmake --install build-linux32 --strip
--- build-cross/build-linux64.sh
+++ build-cross/build-linux64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cmake -S .. -B build-linux64 -DCMAKE_TOOLCHAIN_FILE=${PWD}/linux64.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-linux64 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF &&
+cmake -S .. -B build-linux64 -DCMAKE_TOOLCHAIN_FILE=${PWD}/linux64.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PWD}/install-linux64 -DFIND_GLM=OFF -DFIND_STB=OFF -DUSE_MAGICK=OFF -DUSE_ICU=OFF &&
 cmake --build build-linux64 --parallel $(nproc) &&
 cmake --install build-linux64 --strip
