2bb8bc95b5564b5769133de55d353a6a3a2ccec6
lyazj <seeson@pku.edu.cn>
2026-04-04 23:42:27 +0800

    Fix FontViewer and ImageViewer

--- examples/FontViewer.cpp
+++ examples/FontViewer.cpp
@@ -111,6 +111,6 @@ void GLExampleApplication::UpdateViewer()
     viewerWidth = iWidth * viewerHeight / iHeight;
   }
   renderable->SetRenderable(make_shared<GLRectangle>(viewerWidth, viewerHeight));
-  renderable->SetVertexAttributes(false);
-  renderable->Buffer(false);
+  renderable->SetVertexAttributes(true);
+  renderable->Buffer(true);
 }
--- examples/ImageViewer.cpp
+++ examples/ImageViewer.cpp
@@ -100,6 +100,6 @@ void GLExampleApplication::UpdateViewer()
     viewerWidth = iWidth * viewerHeight / iHeight;
   }
   renderable->SetRenderable(make_shared<GLRectangle>(viewerWidth, viewerHeight));
-  renderable->SetVertexAttributes(false);
-  renderable->Buffer(false);
+  renderable->SetVertexAttributes(true);
+  renderable->Buffer(true);
 }
